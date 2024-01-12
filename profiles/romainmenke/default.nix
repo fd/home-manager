cf162c05c64d649477ee7892117cbdec6ea691c9
Simon Menke <simon.menke@gmail.com>
2024-01-12 17:04:47 +0100

    wip

--- profiles/romainmenke/default.nix
+++ profiles/romainmenke/default.nix
@@ -1,4 +1,18 @@
 { config, pkgs, ... }:
 {
   programs.starship.enable = false;
+  programs.bash.initExtra =
+    ''
+      # Green ';' when exit status is 0, red otherwise.
+      
+      _bash_prompt() {
+          if [[ $? == 0 ]]; then
+              echo -e "\e[32m;\e[0m"
+          else
+              echo -e "\e[31m;\e[0m"
+          fi
+      }
+
+      PS1='$(_bash_prompt)'
+    '';
 }
