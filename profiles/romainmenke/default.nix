d701ce5b14c374ff02ffe086247cd99c415fc8e8
Simon Menke <simon.menke@gmail.com>
2024-01-15 15:18:57 +0100

    wip

--- profiles/romainmenke/default.nix
+++ profiles/romainmenke/default.nix
@@ -4,16 +4,7 @@
   programs.atuin.enable = false;
   programs.bash.initExtra =
     ''
-      # Green ';' when exit status is 0, red otherwise.
-      
-      _bash_prompt() {
-          if [[ $? == 0 ]]; then
-              echo -e "\e[32m;;\e[0m"
-          else
-              echo -e "\e[31m;;\e[0m"
-          fi
-      }
-
-      PS1='$(_bash_prompt)'
+      # Green ';;' when exit status is 0, red otherwise.
+      PS1='$(if [ $? -eq 0 ]; then printf "\[\033[32m\];;\[\033[0m\]"; else printf "\[\033[31m\];;\[\033[0m\]"; fi)'
     '';
 }
